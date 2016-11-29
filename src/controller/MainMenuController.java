8e1082f32fdf5f40d8d28d2040669e05ad91083d
Remy Kaldawy <remykaldawy@gmail.com>
2016-11-28 21:53:10 -0500

    feat: Added LevelBuilderGUI

---
+++ src/controller/MainMenuController.java
@@ -0,0 +1,18 @@
+package controller;
+
+//what am i supposed to do for model and application???
+
+public class MainMenuController {
+	private Model m;
+	private Application a;
+	
+	MainMenuController(Model m, Application a){
+		this.m = m;
+		this.a = a;
+	}
+	
+	public void mousePressed(mouseEvent me){
+		
+	}
+
+}
