a02cac702ae934d1d26a8ed27e02937db1ec43c8
Remy Kaldawy <remykaldawy@gmail.com>
2016-12-05 15:20:57 -0500

    hey this is a test

--- src/model/Board.java
+++ src/model/Board.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 public class Board {
 	Tile[][] tiles = new Tile[6][6];
-	Word selectedTiles;
+	Word selectedTiles; //MAKE SURE THIS STAYS AND YOU USE IT IN THE CONTROLLER
+	
+	//have a nice day
+	heyheyhey
 	
 	public Board(Tile[][] tiles) {
 		this.tiles = tiles;
